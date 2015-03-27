38ec50e32e8fc7d502faccd5b5b6c05b5028aec1
Michael Renner <m.renner@syseleven.de>
2015-03-27 17:32:00 +0100

    os-222 Finalize mcollective support

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,17 +9,39 @@ class sys11mcollective(
 
   case $securityprovider {
     # psk is default upstream, doing nothing
-    # FIXME: empty placeholder?
-    'psk': { $foo = 'bar' }
+    'psk': { }
     'sshkey': { class { 'sys11mcollective::auth::sshkey': } }
     default: { fail('Unsupported security-provider in sys11mcollective') }
   }
 
 
-  class { '::mcollective':
-    securityprovider => $securityprovider,
+  # When we use SSL for the broker mcollective wants all agents to authenticate themselves with certificates as well
+  if hiera('mcollective::middleware_ssl', false) {
+
+    class { '::mcollective':
+      securityprovider   => $securityprovider,
+      ssl_server_public  => "${::puppet_vardir}/ssl/certs/${::fqdn}.pem",
+      ssl_server_private => "${::puppet_vardir}/ssl/private_keys/${::fqdn}.pem",
+      ssl_ca_cert        => "${::puppet_vardir}/ssl/certs/ca.pem",
+    }
+
+  }
+  else {
+    class { '::mcollective':
+      securityprovider  => $securityprovider,
+    }
+  }
+
+  # When we use SSL for the broker mcollective wants all clients to authenticate themselves with certificates as well
+  if hiera('mcollective::middleware_ssl', false) and hiera('mcollective::client', false) {
+    mcollective::client::setting { 'plugin.rabbitmq.pool.1.ssl.cert':
+      value => "${mcollective::confdir}/server_public.pem",
+    }
+
+    mcollective::client::setting { 'plugin.rabbitmq.pool.1.ssl.key':
+      value => "${mcollective::confdir}/server_private.pem",
+    }
   }
 
   class { 'sys11mcollective::plugins': }
-
 }
